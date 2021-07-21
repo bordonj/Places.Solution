e96c85bbf10d26384ef98ce6d9f6d20de9c3313c
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-21 12:54:38 -0700

    add logic to pass test for properties in place.cs
    Co-authored-by: Erika Debelis <erika.debelis@gmail.com>

--- Places/Models/Place.cs
+++ Places/Models/Place.cs
@@ -5,11 +5,23 @@ namespace Places.Models
   public class Place
   {
     public string PlaceName { get; set; }
+    public string LengthOfStay { get; set; }
+    public string Company { get; set; }
+    public string JournalEntry { get; set; }
+    public string DateOfVisit { get; set; }
+    public string FavFood { get; set; }
+// PlaceName
+// Company
+// LengthOfStay
+// DateOfVisit
+// JournalEntry
+// FavFood
+
     public int Id { get; } // don't add set method, bc this property will be set in the constructor automatically
     // don't EVER want to manually edit it bc it might lose uniqueness
     private static List<Place> _instances = new List<Place> {};
 
-    public Place (string placeName)
+    public Place (string placeName, string company, string lengthOfStay, string dateOfVisit, string journalEntry, string favFood)
     {
       PlaceName = placeName;
       _instances.Add(this);
